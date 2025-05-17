7b683196c001d55a350acfb1f272a258388c1eb0
Matheus <matheusholv@gmail.com>
2025-05-17 15:35:48 -0300

    fixed convex config and setup convex deploy to production

--- convex.config.js
+++ convex.config.js
@@ -1,49 +1,23 @@
 // @ts-check
 
-/** @type {import('convex').Config} */
 module.exports = {
   // When running `npx convex dev` or `npx convex deploy`,
   // this configures the URL of your Convex deployment.
-  // You can also set this to "http://localhost:3000" to test locally.
-  //
-  // When you're ready to deploy, update this to your production URL.
-  // For example, if your Convex deployment is at "https://happy-otter-123.convex.cloud",
-  // set this to "https://happy-otter-123.convex.cloud".
-  //
-  // You can find your deployment URL by running `npx convex dashboard`.
-  //
-  // If you don't have a Convex deployment yet, run `npx convex dev` to create one.
-  //
-  // When you're ready to deploy to production, run `npx convex deploy`.
-  //
-  // Learn more at https://docs.convex.dev/quick-start
-  //
-  // When you're ready to deploy, update this to your production URL.
-  // For example, if your Convex deployment is at "https://happy-otter-123.convex.cloud",
-  // set this to "https://happy-otter-123.convex.cloud".
-  //
-  // You can find your deployment URL by running `npx convex dashboard`.
-  //
-  // If you don't have a Convex deployment yet, run `npx convex dev` to create one.
-  //
-  // When you're ready to deploy to production, run `npx convex deploy`.
-  //
-  // Learn more at https://docs.convex.dev/quick-start
+  // Set to undefined to use the default Convex deployment for this project.
   deployment: undefined,
+
   // Enable this to run Convex in development mode.
-  // When enabled, Convex will use a local development server instead of connecting to Convex Cloud.
-  //
-  // Set this to false to connect to your production deployment.
-  //
-  // Learn more at https://docs.convex.dev/quick-start#local-development
-  development: true,
+  // When true, Convex will use a local development server.
+  // Set to false to connect to your production deployment.
+  development: process.env.NODE_ENV !== 'production',
+
   // The directory where Convex will store its development data.
   // This is relative to the directory containing this config file.
   dataDir: ".convex",
+  
   // The directory where Convex will store its logs.
-  // This is relative to the directory containing this config file.
   logDir: ".convex/logs",
+  
   // The directory where Convex will store its cache.
-  // This is relative to the directory containing this config file.
   cacheDir: ".convex/cache",
 };
